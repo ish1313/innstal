707d4fdf19817b1e2901663ee23462c8ab892eed
K Md Mamunur Rashid <mamun1980@gmail.com>
2017-10-23 17:07:11 +0600

    added signup, login and dashboard layout

--- innstal/settings.py
+++ innstal/settings.py
@@ -66,16 +66,16 @@ WSGI_APPLICATION = 'innstal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'innstal2',
-        'USER': 'innstal',
-        'PASSWORD': 'qweqwe',
-        'HOST': '192.168.43.58',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'innstal2',
+#         'USER': 'innstal',
+#         'PASSWORD': 'qweqwe',
+#         'HOST': '192.168.43.58',
+#         'PORT': '5432',
+#     }
+# }
 
 import dj_database_url
 
