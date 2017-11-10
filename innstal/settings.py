329a9c4c53b87f9e03b49553f8e0c997d0f05ecb
K Md Mamunur Rashid <mamun1980@gmail.com>
2017-11-10 18:28:51 +0600

    working with templates

--- innstal/settings.py
+++ innstal/settings.py
@@ -81,13 +81,13 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-#
-# ENVIRONMENT = 'production'
-#
-# DATABASES['default'] = dj_database_url.config(
-#     default='postgres://gmeqyjfuhsaxtw:2dc36a5fd8a4b3d93b1af7eb230f1390e9eaf17622710b53e7e288b1366853e2@ec2-54-235-90-125.compute-1.amazonaws.com:5432/d7rlg505efksfp'
-# )
+import dj_database_url
+
+ENVIRONMENT = 'production'
+
+DATABASES['default'] = dj_database_url.config(
+    default='postgres://gmeqyjfuhsaxtw:2dc36a5fd8a4b3d93b1af7eb230f1390e9eaf17622710b53e7e288b1366853e2@ec2-54-235-90-125.compute-1.amazonaws.com:5432/d7rlg505efksfp'
+)
 
 
 # Password validation
